0ed325b158a2ada7347096ba2153837adda7cf68
Alok Raj <alokr417@gmail.com>
2021-01-19 17:55:10 +0530

    main comp getting result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './services/user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-random-card';
+export class AppComponent implements OnInit {
+  user: any;
+
+  constructor(private userService: UserService, private toastr: ToastrService) {}
+
+  ngOnInit() {
+    this.userService.getUser().subscribe((user: any) => {
+      this.user = user.results[0];
+    }, (err) => {
+      this.toastr.error(err.status, 'Opps');
+    });
+  }
 }
